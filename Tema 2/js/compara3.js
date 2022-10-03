af3e34a0e604eefe1ea44d9e85b11c4dfc613c97
fran-dawbaza <fgargar092@g.educaand.es>
2022-10-03 12:32:02 +0200

    compara3

--- Tema 2/js/compara3.js
+++ Tema 2/js/compara3.js
@@ -2,10 +2,12 @@ function compara3 (evento){
     evento.preventDefault();
     const n1 = +document.getElementById('n1').value;
     const n2 = +document.getElementById('n2').value;
+    const n3 = +document.getElementById('n3').value;
 
     console.log(n1);
 
-    document.getElementById('resultado').innerHTML ='n1 es mayor que n2 y que n3';
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML ='n1 es mayor que n2 y n2 mayor que n3';
 }
 
 document
