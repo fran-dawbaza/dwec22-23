2929a2f67aa9d5185ee79df1a4d5cea5b1aac9df
fran-dawbaza <fgargar092@g.educaand.es>
2022-10-14 12:45:51 +0200

    funciones

--- Tema 2/js/funciones.js
+++ Tema 2/js/funciones.js
@@ -6,8 +6,22 @@ function saluda(persona) {
     alert('Buenos días '+ persona);
 }
 
-saluda2('Manolito');
+// En la siguiente línea se produce error, por eso la comento
+//saluda2('Manolito');
 
 const saluda2 = function (persona) {
     alert('Buenos días '+ persona);
 };
+
+/* Los callbacks son funciones que tienen como parámetro otra función. 
+Los callbacks aseguran que una función no se va a ejecutar antes de que se 
+complete una tarea, sino que se ejecutará justo después de que la tarea 
+se haya completado.Los callbacks tienen 3 usos principales:
+1.- Realizar tareas asíncronas
+    Por ejemplo para la gestión de eventos (hacer algo cuando se produzca
+    un evento determinado).
+2.- Programación funcional
+    Por ejemplo
+        array.filter(elemento=>elemento>100); 
+3.- Evitar depedencias entre funciones
+*/
