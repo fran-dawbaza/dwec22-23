4250a65ed97ab24e8cb3eab194451281f2824fac
fran-dawbaza <fgargar092@g.educaand.es>
2022-10-20 10:00:33 +0200

    fibonacci y fizzbuzz

--- Tema 2/js/bucles.js
+++ Tema 2/js/bucles.js
@@ -57,3 +57,41 @@ pintaTrianguloFuncional(4);
 
 console.log('Llamada con parámetro -1: ');
 pintaTrianguloFuncional(-1);
+
+
+function fibonacciIterativo(posicion) {
+    if (posicion < 1) return 0;
+    if (posicion < 3) return 1;
+    
+    let n = 0;
+    for (let n_1 = 1, n_2 = 1, i=2;i<posicion;i++){
+        n = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = n;
+    }
+    return n;
+}
+
+function fibonacciRecursivo(posicion) {
+    if (posicion < 1n) return 0;
+    if (posicion < 3n) return 1;
+    return fibonacciRecursivo(posicion-1) + fibonacciRecursivo(posicion-2);
+}
+
+
+
+console.log('Debe dar 1: '+ fibonacciIterativo(2));
+console.log('Debe dar 2: '+ fibonacciIterativo(3));
+console.log('Debe dar 8: '+ fibonacciIterativo(6));
+console.log('Debe dar 21: '+ fibonacciIterativo(8));
+console.time('fiboIterativo50');
+console.log('fibonnacci(1000): '+ fibonacciIterativo(1000));
+console.timeEnd('fiboIterativo50');
+//console.log('fibonnacci(100000): '+ fibonacciIterativo(100000));
+console.log('Debe dar 1: '+ fibonacciRecursivo(2));
+console.log('Debe dar 2: '+ fibonacciRecursivo(3));
+console.log('Debe dar 8: '+ fibonacciRecursivo(6));
+console.log('Debe dar 21: '+ fibonacciRecursivo(8));
+console.time('fiboRecursivo50');
+console.log('fibonnacci(50): '+ fibonacciRecursivo(50));
+console.timeEnd('fiboRecursivo50');
