1ac7083f8cec1b3da5ae14951d6def0a387f66a5
fran-dawbaza <fgargar092@g.educaand.es>
2023-02-10 10:27:24 +0100

    importar json con await

--- Tema 4/js/importarjson.js
+++ Tema 4/js/importarjson.js
@@ -1,6 +1,22 @@
-import {main} from "./main.js";
-fetch("./asset/productos.json")
+//import {main} from "./main.js";
+
+/*fetch("./asset/productos.json")
     .then(result=>result.json())
     .then(productos=>{
         main(productos);
-    });
+    });*/
+
+const resultado = await fetch("./asset/productos.json");
+const productos = await resultado.json();
+
+//main(productos);
+
+const productosBaratos20 = productos.filter(p=>p.precio<20);
+    const listadoProductos = '<ul>' 
+                            + productosBaratos20
+                                        .map(p=>`<li>${p.nombre}</li>`)
+                                        .join('')
+                            + '</ul>';
+    document.getElementById('datosDeJSON').innerHTML = listadoProductos;
+
+
