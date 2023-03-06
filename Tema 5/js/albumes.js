830abb4a69956e8af26d9a970cba49c2ca45c9be
fran-dawbaza <fgargar092@g.educaand.es>
2023-03-06 11:48:57 +0100

    albumes

--- Tema 5/js/albumes.js
+++ Tema 5/js/albumes.js
@@ -6,15 +6,14 @@ function muestraUsuarios(){
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(resultado=>resultado.json())
     .then(usuarios=>{
-        contenedor.innerHTML = '<h2>Lista de usuarios</h2><ul>' + 
-                                usuarios.map(
-        u=>`
-        <li>
-            <span>${u.name}</span>
-            <span><button onclick="muestraAlbumesPorUsuario(${u.id},'${u.name}')">ver álbumes</button></span> 
-        </li>
-        `).join('') +
-                                '</ul>';
+        const liUsuarios= usuarios.map(u=>`
+            <li>
+                <span>${u.name}</span>
+                <span><button onclick="muestraAlbumesPorUsuario(${u.id},'${u.name}')">ver álbumes</button></span> 
+            </li>
+            `).join('');
+        contenedor.innerHTML = `<h2>Lista de usuarios</h2>
+                                <ul>${liUsuarios}</ul>`;
     })
     .catch(error=>{
         alert(error);
@@ -26,17 +25,19 @@ function muestraAlbumesPorUsuario(idUsuario,nombreUsuario){
     fetch("https://jsonplaceholder.typicode.com/albums")
     .then(resultado=>resultado.json())
     .then(albums=>{
-        contenedor.innerHTML = `<h2>Álbumes de ${nombreUsuario}</h2><ul>` + 
-                                albums
-                                .filter(a=>a.userId===idUsuario)
-                                .map(a=>`
-        <li>
-            <span>${a.title}</span>
-            <span><button onclick="muestraFotosPorAlbum(${a.id}, ${idUsuario},'${nombreUsuario}')">ver fotos</button></span> 
-        </li>
-        `).join('') +
-                                `</ul>
-                                <div><button onclick="muestraUsuarios()">Volver</button>`;
+        const liAlbumes = albums
+                            .filter(a=>a.userId===idUsuario)
+                            .map(a=>`
+            <li>
+                <span>${a.title}</span>
+                <span>
+                    <button onclick="muestraFotosPorAlbum(${a.id}, ${idUsuario},'${nombreUsuario}')">ver fotos</button>
+                </span> 
+            </li>`).join('');
+        contenedor.innerHTML = `
+            <h2>Álbumes de ${nombreUsuario}</h2>
+            <ul>${liAlbumes}</ul>
+            <div><button onclick="muestraUsuarios()">Volver</button>`;
     })
     .catch(error=>{
         alert(error);
@@ -48,18 +49,19 @@ function muestraFotosPorAlbum(idAlbum, idUsuario,nombreUsuario){
     fetch("https://jsonplaceholder.typicode.com/photos")
     .then(resultado=>resultado.json())
     .then(photos=>{
+        const liFotos = photos
+                            .filter(f=>f.albumId===idAlbum)
+                            .map(f=>`
+                                <li>
+                                    <span>${f.title}</span>
+                                    <span><img src="${f.url}"/></span>            
+                                </li>`).join('');
         contenedor.innerHTML = `
-        <h2>Fotos de ${nombreUsuario} del álbum ${idAlbum}</h2>
-        <div><button onclick="muestraAlbumesPorUsuario(${idUsuario},'${nombreUsuario}')">Volver</button></div>
-        <ul>` + 
-                                photos
-                                .filter(f=>f.albumId==idAlbum)
-                                .map(f=>`
-        <li>
-            <span>${f.title}</span>
-            <span><img src="${f.url}"/></span>            
-        </li>
-        `).join('') +  '</ul>';
+                <h2>Fotos de ${nombreUsuario} del álbum ${idAlbum}</h2>
+                <div>
+                    <button onclick="muestraAlbumesPorUsuario(${idUsuario},'${nombreUsuario}')">Volver</button>
+                </div>
+                <ul>${liFotos}</ul>`;
     })
     .catch(error=>{
         alert(error);
